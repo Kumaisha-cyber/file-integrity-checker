b32455e84788fc63b45a159825358e575fb0a455
Kumaisha-cyber <Kumaishakumaisha86@gmail.com>
2026-07-23 18:02:04 +0000

    Add safe and compromised detection

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import hashlib
 
 app = Flask(__name__)
 
+original_hash = None
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -11,15 +14,33 @@ def home():
 @app.route("/check", methods=["POST"])
 def check_file():
 
+    global original_hash
+
     file = request.files["file"]
 
     file_data = file.read()
 
     current_hash = hashlib.sha256(file_data).hexdigest()
 
-    return jsonify({
-        "message": f"File checked successfully!\nSHA-256 Hash: {current_hash}"
-    })
+    if original_hash is None:
+
+        original_hash = current_hash
+
+        return jsonify({
+            "message": "ORIGINAL HASH SAVED\n\nFILE INTEGRITY: SAFE\n\nSHA-256 Hash: " + current_hash
+        })
+
+    elif current_hash == original_hash:
+
+        return jsonify({
+            "message": "FILE INTEGRITY: SAFE\n\nFile has not been modified.\n\nSHA-256 Hash: " + current_hash
+        })
+
+    else:
+
+        return jsonify({
+            "message": "FILE INTEGRITY: COMPROMISED\n\nFile has been modified!\n\nSHA-256 Hash: " + current_hash
+        })
 
 
 if __name__ == "__main__":
