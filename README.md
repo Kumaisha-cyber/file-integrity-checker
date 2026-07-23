937acce146bc41539d32bf53f475708d4bba7e17
Kumaisha-cyber <Kumaishakumaisha86@gmail.com>
2026-07-23 15:31:58 +0000

    Improve file integrity checker project

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# file-integrity-checker
+# File Integrity Checker 🔐
+
 A Python cybersecurity tool that detects file modifications using SHA-256 hashing.
+
+## Features
+
+- Calculates SHA-256 hash of a file
+- Detects unauthorized file modifications
+- Compares original and current file hashes
+- Displays SAFE or COMPROMISED status
+
+## Technologies Used
+
+- Python
+- SHA-256
+- hashlib
+
+## How to Run
+
+Run the following command:
+
+python file_integrity_checker.py
+
+Then enter the file name:
+
+test.txt
+
+## How It Works
+
+The program creates an original SHA-256 hash of the file.
+
+After the file is modified, the program creates a new SHA-256 hash.
+
+If both hashes are the same:
+
+FILE INTEGRITY: SAFE
+
+If the hashes are different:
+
+FILE INTEGRITY: COMPROMISED
+
+## Cybersecurity Concepts
+
+- File Integrity Monitoring
+- Cryptographic Hashing
+- SHA-256
+- Data Integrity
+- Tamper Detection
--- file_integrity_checker.py
+++ file_integrity_checker.py
@@ -1,27 +1,45 @@
 import hashlib
-
+import os
 
 def calculate_hash(filename):
-
-    sha256_hash = hashlib.sha256()
+    sha256 = hashlib.sha256()
 
     with open(filename, "rb") as file:
+        while True:
+            data = file.read(4096)
 
-        for byte_block in iter(
-            lambda: file.read(4096),
-            b""
-        ):
+            if not data:
+                break
 
-            sha256_hash.update(byte_block)
+            sha256.update(data)
 
-    return sha256_hash.hexdigest()
+    return sha256.hexdigest()
 
 
-filename = input(
-    "Enter the file path: "
-)
+filename = input("Enter the file path: ")
 
-file_hash = calculate_hash(filename)
+if os.path.exists(filename):
 
-print("\nSHA-256 Hash:")
-print(file_hash)
+    original_hash = calculate_hash(filename)
+
+    print("\nOriginal SHA-256 Hash:")
+    print(original_hash)
+
+    print("\nNow modify the file if you want to test integrity.")
+    input("Press Enter after modifying the file...")
+
+    current_hash = calculate_hash(filename)
+
+    print("\nCurrent SHA-256 Hash:")
+    print(current_hash)
+
+    if original_hash == current_hash:
+        print("\nFILE INTEGRITY: SAFE")
+        print("The file has not been modified.")
+
+    else:
+        print("\nFILE INTEGRITY: COMPROMISED")
+        print("The file has been modified!")
+
+else:
+    print("File not found!")
